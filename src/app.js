0a03c9ef0b27daff0ec39f45d6ea27f186c13291
hscott13 <hscott13@hotmail.co.uk>
2023-02-04 11:24:44 +0000

    Added current location option

--- src/app.js
+++ src/app.js
@@ -12,6 +12,9 @@
 
 function updateCity(event) {
   let cityTimeZone = event.target.value;
+  if (cityTimeZone === "current") {
+    cityTimeZone = moment.tz.guess();
+  }
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
   let cityBlock = document.querySelector("#cityBlock");
